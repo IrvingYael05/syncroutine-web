ab901b615f3a6e93fbfb1ca3ca1a0cbe5f834ff4
IrvingYael05 <irving.vazser@gmail.com>
2026-07-24 00:01:33 -0600

    fix: update apiUrl to include '/api' in environment configuration

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ export const environment = {
   supabaseUrl: 'https://lbzhgdapghedavirdhrp.supabase.co',
   supabaseKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxiemhnZGFwZ2hlZGF2aXJkaHJwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODI0OTk0NjIsImV4cCI6MjA5ODA3NTQ2Mn0.CR7tJYGm71Abkk-rlYcVNwt_D1Gi7nBa9jAyKivRG28',
-  apiUrl: 'https://syncroutine-api.onrender.com',
+  apiUrl: 'https://syncroutine-api.onrender.com/api',
 };
